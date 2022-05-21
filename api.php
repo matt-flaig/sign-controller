80212e4661546eae54aad02e903df0874b62bc16
matt-flaig <hi@mattflaig.com>
2022-05-20 22:09:22 -0700

    Modified project to use git submodules

--- api.php
+++ api.php
@@ -1,86 +1,96 @@
 <?php
-/**
- * Copyright (c) 2017 D. Dante Lorenso <dante@lorenso.com>.  All Rights Reserved.
- * This source file is subject to the MIT license that is bundled with this package
- * in the file LICENSE.txt.  It is also available at: https://opensource.org/licenses/MIT
- */
-
-require 'bootstrap.php';
-
-use LarkSpark\ArtNet\ArtDmxClient;
-
-$dmxData = [];
-
-$data = $_POST;
-$command = $data["command"];
-if($command == "dmx"){
-    $dmx = $data["dmx"];
-    if(!$dmx){
-        exit(json_encode("dmx_required"));
-    }
-
-    try{
-        $channelCount = 1; //dmx universes start at channel one!!!
-        $dmxChannelValues = [];
-        $dmxData = [];
-        foreach($dmx as $fixture){
-            foreach($fixture as $channel){
-                $dmxChannelValues[$channelCount] = $channel;
-                array_push($dmxData, $channel);
-                $channelCount++;
+    
+    // class auto-loader
+    spl_autoload_register(
+        function ($class_name) {
+            // PSR-4: convert class name to file name
+            $lib_file = 'ls-artdmx-php/library/' . strtr(ltrim($class_name, '\\'), ['\\' => '/']) . '.php';
+            if (file_exists($lib_file)) {
+                include $lib_file;
+                return true;
             }
+            
+            // not found!
+            return false;
         }
-    } catch (Exception $e) {
-        exit(json_encode("invalid_request"));
+    );
+    
+    use LarkSpark\ArtNet\ArtDmxClient;
+    
+    $dmxData = [];
+    
+    $data = $_POST;
+    $command = $data["command"] ?? [];
+    if($command == "dmx"){
+        $dmx = $data["dmx"] ?? [];
+        if(!$dmx){
+            exit(json_encode("dmx_required"));
+        }
+    
+        try{
+            $channelCount = 1; //dmx universes start at channel one!!!
+            $dmxChannelValues = [];
+            $dmxData = [];
+            foreach($dmx as $fixture){
+                foreach($fixture as $channel){
+                    $dmxChannelValues[$channelCount] = $channel;
+                    array_push($dmxData, $channel);
+                    $channelCount++;
+                }
+            }
+        } catch (Exception $e) {
+            exit(json_encode("invalid_request"));
+        }
     }
-}
-if($data["presets"]){
-    $presets = $data["presets"];
-    if(!$presets){
-        exit(json_encode("presets_required"));
+    if(array_key_exists("presets", $data)){
+        $presets = $data["presets"];
+        if(!$presets){
+            exit(json_encode("presets_required"));
+        }
+        
+        try{
+            $channelCount = 1; //dmx universes start at channel one!!!
+            $dmxChannelValues = [];
+            $dmxData = [];
+            foreach($presets[$data["currentPreset"]] as $fixture){
+                foreach($fixture as $channel){
+                    $dmxChannelValues[$channelCount] = $channel;
+                    array_push($dmxData, $channel);
+                    $channelCount++;
+                }
+            }
+        } catch (Exception $e) {
+            exit(json_encode("invalid_request"));
+        }
+    
+        // loop through provided presets
+        // update json file
+        // update art-net device
     }
+    
     
     try{
-        $channelCount = 1; //dmx universes start at channel one!!!
-        $dmxChannelValues = [];
-        $dmxData = [];
-        foreach($presets[$data["currentPreset"]] as $fixture){
-            foreach($fixture as $channel){
-                $dmxChannelValues[$channelCount] = $channel;
-                array_push($dmxData, $channel);
-                $channelCount++;
-            }
-        }
+        $client = new ArtDmxClient('169.254.188.27', 6454);
+    
+    //169.254.255.255
+    
+        /*
+        ip tests
+        169.254.58.97
+        169.254.255.255
+        224.0.0.251
+        
+        **169.254.255.255**
+        */
+    
+        // broadcast our DMX values
+        
+        // the broadcast ends up just being a big array with key > value (channel > intensity) pairs.
+        $client->broadcast($dmxData);
     } catch (Exception $e) {
-        exit(json_encode("invalid_request"));
+        //print_r($e);
+        exit(json_encode("art-net_failure"));
     }
-
-    // loop through provided presets
-    // update json file
-    // update art-net device
-}
-
-
-try{
-   $client = new ArtDmxClient('169.254.188.27', 6454);
-
-//169.254.255.255
-
-    /*
-    ip tests
-    169.254.58.97
-    169.254.255.255
-    224.0.0.251
+    echo json_encode("success");
     
-    **169.254.255.255**
-    */
-
-    // broadcast our DMX values
-    
-    // the broadcast ends up just being a big array with key > value (channel > intensity) pairs.
-    $client->broadcast($dmxData);
-} catch (Exception $e) {
-    print_r($e);
-    exit(json_encode("art-net_failure"));
-}
-echo json_encode("success");
+?>
